7896dcafdfa85b7b18ada43194850f3429299d88
Sooyeon <109861031+izayaki22@users.noreply.github.com>
2024-04-13 13:47:38 +0900

    Title: 코드트리 채점기, Time: 5050ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240413/코드트리 채점기/codetree-judger.py
+++ 240413/코드트리 채점기/codetree-judger.py
@@ -5,11 +5,14 @@ import heapq
 q = int(input())
 cmd, n, u = input().split()
 n = int(n)
+
 judgelist = [0] * (n + 1)  # 채점기 list
 heap = []  # 채점대기큐 (p, t, domain/id)
 waiting_url = {}  # {domain/id : 1}
 doing = {}  # 채점진행 dict {domain : start}
 finish = {}  # 채점완료 dict {domain : start + 3 * gap}
+
+ans = 1
 heapq.heappush(heap, (1, 0, u))
 waiting_url[u] = 1
 
@@ -23,9 +26,9 @@ for _ in range(1, q):
         if(u not in waiting_url):
             heapq.heappush(heap, (p, t, u))
             waiting_url[u] = 1
+            ans += 1
 
     elif (cmd_list[0] == '300'):
-    
         t = int(cmd_list[1]) # 채점 start 시간
         total = len(heap)
 
@@ -46,6 +49,7 @@ for _ in range(1, q):
                                 judgelist[i] = ndom
                                 doing[ndom] = t
                                 del waiting_url[nu]
+                                ans -= 1
                                 break
                         break
                     else:
@@ -56,6 +60,7 @@ for _ in range(1, q):
                             judgelist[i] = ndom
                             doing[ndom] = t
                             del waiting_url[nu]
+                            ans -= 1
                             break
                     break
 
@@ -67,11 +72,10 @@ for _ in range(1, q):
         t, jid = map(int, cmd_list[1:])
         dom = judgelist[jid]
         judgelist[jid] = 0
-
         if(dom in doing):
             start = doing[dom]
             finish[dom] = start + 3 * (t - start)
             del doing[dom]
 
     else:
-        print(len(heap))
+        print(ans)
